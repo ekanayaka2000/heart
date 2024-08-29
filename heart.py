61cb98a0288c17ec07275c2b0a77ec3a56aba593
Pramod Ekanayaka <128894685+ekanayaka2000@users.noreply.github.com>
2024-08-29 23:51:03 +0530

    update

--- heart.py
+++ heart.py
@@ -1,21 +1,24 @@
 import math
-from turtle import*
+from turtle import *
 
 def heartA(k):
-    return 15*math.sin(k)**3
+    return 15 * math.sin(k) ** 3
 
 def heartB(k):
-    return 12*math.cos(k)-5*\
-    math.cos(2*k)-2-\
-    math.cos(3*k)-\
-    math.cos(4*k)
-    
-speed(9000)
-bgcolor("white")
+    return 12 * math.cos(k) - 5 * \
+        math.cos(2 * k) - 2 * \
+        math.cos(3 * k) - \
+        math.cos(4 * k)
+
+speed(0)
+bgcolor("black")
+color("red")
+penup()
 
 for i in range(6000):
-    goto(heartA(i)*20,heartB(i)*20)
-    for j in range(5):
-        color("red")
-    goto(0,0)
+    x = heartA(i) * 20
+    y = heartB(i) * 20
+    goto(x, y)
+    pendown()
+
 done()
